52e8428c81a9eef3424cf30e5ef16fcd21a268f5
Nicolas McCurdy <thenickperson@gmail.com>
2013-07-01 20:07:36 -0400

    Make jslint happy with cookies.js

--- cookies.js
+++ cookies.js
@@ -1,31 +1,43 @@
 // Based off of http://www.quirksmode.org/js/cookies.html
 
+/*jslint browser: true */
+/*jslint indent: 2 */
+
+"use strict";
+
 var Cookies = {
 
   set: function (name, value, days) {
+    var date,
+      expires = "";
     if (days) {
-      var date = new Date();
-      date.setTime(date.getTime()+(days*24*60*60*1000));
-      var expires = "; expires="+date.toGMTString();
+      date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toGMTString();
     }
-    else var expires = "";
-    document.cookie = name+"="+value+expires+"; path=/";
+    document.cookie = name + "=" + value + expires + "; path=/";
     return this;
   },
 
   get: function (name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-      var c = ca[i];
-      while (c.charAt(0)==' ') c = c.substring(1,c.length);
-      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    var nameEQ = name + "=",
+      ca = document.cookie.split(';'),
+      i,
+      c;
+    for (i = 0; i < ca.length; i += 1) {
+      c = ca[i];
+      while (c.charAt(0) === ' ') {
+        c = c.substring(1, c.length);
+      }
+      if (c.indexOf(nameEQ) === 0) {
+        return c.substring(nameEQ.length, c.length);
+      }
     }
     return null;
   },
 
   clear: function (name) {
-    Cookies.set(name,"",-1);
+    Cookies.set(name, "", -1);
     return this;
   }
 
